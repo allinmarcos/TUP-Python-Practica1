602f78443e3b94f4d0e2493b108db987a8637af5
TacheroCósmico <marcosch.allin@gmail.com>
2022-04-04 20:04:19 -0300

    Hasta el cuatro, 2 con errores, 5 incompleto

--- exercises/exercise2.py
+++ exercises/exercise2.py
@@ -6,11 +6,14 @@ Construir una expresión lógica que use TODAS las variables y cuyo resultado se
 True si al menos una de las variables es True.
 """
 
+from xml.sax.saxutils import prepare_input_source
+
+
 esta_lloviendo = True
 riego_activado = True
 
 # COMPLETAR - INICIO
-
+piso_mojado = esta_lloviendo or riego_activado
 # COMPLETAR - FIN
 
 assert piso_mojado
@@ -26,7 +29,7 @@ lado_cuadrado = 5
 area_cuadrado = pow(lado_cuadrado, 2)
 
 # COMPLETAR - INICIO
-
+area_mayor_a_cinco = not area_cuadrado <=5
 # COMPLETAR - FIN
 
 assert area_mayor_a_cinco
@@ -41,7 +44,7 @@ numero_1 = 49
 numero_2 = 50
 
 # COMPLETAR - INICIO
-
+resultado = (numero_1 %7==0) and (numero_2%7!=0) #resultado = (numero_1 %7==0)or(numero_2%7==0)
 # COMPLETAR - FIN
 
 assert resultado
@@ -60,7 +63,7 @@ variable_04 = "90"
 variable_05 = 100
 
 # COMPLETAR - INICIO
-
+80= #mecanismo de cortocircuito???
 # COMPLETAR - FIN
 
 assert resultado == 80
--- exercises/exercise3.py
+++ exercises/exercise3.py
@@ -12,7 +12,7 @@ persona_02 = "Kevin"
 edad_02 = 41
 
 # COMPLETAR - INICIO
-
+comparar_nombre_y_edad = (persona_01==persona_02) and (edad_01!=edad_02)
 # COMPLETAR - FIN
 
 assert comparar_nombre_y_edad
@@ -28,7 +28,7 @@ marca_del_auto = "Chevrolet"
 modelo_de_auto = 1998
 
 # COMPLETAR - INICIO
-
+comparar_marca_y_modelo = (marca_del_auto != "Ford") and (modelo_de_auto<=2000)
 # COMPLETAR - FIN
 
 assert comparar_marca_y_modelo
@@ -46,7 +46,7 @@ superficie_de_campo_02 = 851212
 superficie_de_campo_03 = 8512
 
 # COMPLETAR - INICIO
-
+comparar_superficie = (superficie_de_campo_01 < superficie_de_campo_02 > superficie_de_campo_03)
 # COMPLETAR - FIN
 
 assert comparar_superficie
@@ -54,9 +54,14 @@ assert comparar_superficie
 
 """
 Construir una expresión lógica que use TODAS las variables y cuyo resultado sea
-True si la cantidad de bananas es menor a la mitad de la cantidad de naranjas,
+True si
+
+ la cantidad de bananas es menor a la mitad de la cantidad de naranjas,
+
 la mitad de naranjas es menor a dos veces la cantidad de manzanas y dos veces
 la cantidad de manzanas es menor o igual a la cantidad de peras al cuadrado.
+
+
 Restricción: Utilizar comparaciones encadenadas y no utilizar and, or ni not.
 """
 
@@ -66,7 +71,7 @@ manzanas = 300
 peras = 30
 
 # COMPLETAR - INICIO
-
+comparar_frutas = (bananas < naranjas/2 < manzanas*2 <= peras**2)
 # COMPLETAR - FIN
 
 assert comparar_frutas
